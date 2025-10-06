ca70162474912ce5ccb96728f64d82e43a4a9019
Sharhab <muktarsharhadev@gmail.com>
2025-10-06 17:23:16 +0400

    ppp

--- src/server.js
+++ src/server.js
@@ -23,6 +23,8 @@ async function startServer() {
   // Security
   app.use(helmet());
   app.use(express.json({ limit: "10mb" }));
+  app.use(express.urlencoded({ extended: true }));
+
 
   // CORS
   app.use(
