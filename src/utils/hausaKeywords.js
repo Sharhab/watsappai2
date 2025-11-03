339e82deec5fcbe8f8b487670222fef78613d8e6
Sharhab <muktarsharhadev@gmail.com>
2025-11-03 10:40:27 +0400

    now up

---
+++ src/utils/hausaKeywords.js
@@ -0,0 +1,14 @@
+export const HAUSA_KEYWORDS = {
+  // Men's sexual performance
+  "karfin gaba": ["karfin maza", "karfin namiji", "karfin jiki", "karfin aure", "gaba ya yi rauni", "gaba bata tashi"],
+  "saurin inzali": ["cumming fast", "saurin kawowa", "release quickly", "jayayya ta gaggawa", "rash medical"],
+  "alaura": ["girma", "raunin gaba", "istiminai", "wasa da al aura", "tsawon gaba", "ara girma", "karin girman gaba"],
+
+  // Infections / toilet issues
+  "infection": ["sanyi", "kuraje", "ciwon al'aura", "kazanta", "wari", "ciwon gaba"],
+  "toilet problem": ["toilet infection", "fitsari yana ciwo", "fitsari zafi", "yawan fitsari", "fitsari da daddare"],
+
+  // Women related
+  "karin sha'awa mace": ["matsa gaban mace", "karfin mace", "karfin sha'awa", "mace bata ji dadi"],
+  "tsarki / period": ["al'ada", "period", "jinin haila", "jinin ba ya tsayawa"],
+};
