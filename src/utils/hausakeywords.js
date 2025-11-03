803808b37ab09f99aaa731413dba5ee9aea6c065
Sharhab <muktarsharhadev@gmail.com>
2025-11-03 10:35:24 +0400

    change

--- src/utils/hausakeywords.js
+++ src/utils/hausakeywords.js
@@ -2,7 +2,7 @@ export const HAUSA_KEYWORDS = {
   // Men's sexual performance
   "karfin gaba": ["karfin maza", "karfin namiji", "karfin jiki", "karfin aure", "gaba ya yi rauni", "gaba bata tashi"],
   "saurin inzali": ["cumming fast", "saurin kawowa", "release quickly", "jayayya ta gaggawa", "rash medical"],
-  "alaura": ["girma", "raunin gaba", "tsawon gaba", "ara girma", "karin girman gaba"],
+  "alaura": ["girma", "raunin gaba", "istiminai", "wasa da al aura", "tsawon gaba", "ara girma", "karin girman gaba"],
 
   // Infections / toilet issues
   "infection": ["sanyi", "kuraje", "ciwon al'aura", "kazanta", "wari", "ciwon gaba"],
